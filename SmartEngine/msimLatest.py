667ae6647c97295d76bc07e59407c4643823542d
Vinay D Gor <vinaygor11@gmail.com>
2019-11-18 20:09:04 +0000

    Add files via upload

--- SmartEngine/msimLatest.py
+++ SmartEngine/msimLatest.py
@@ -10,65 +10,90 @@ html = urlopen("https://www.morganstanley.com/im/en-us/institutional-investor/in
 soup = BeautifulSoup(html, 'html.parser')
 # print(soup)
 # Result Items Container
-tileContainer = soup.findAll('div', {"class": "insightArticleTile"})
+tileContainer = soup.findAll('a', {"class": "pressCenterLink insightsIndexList"})
 print("Len of tile container",len(tileContainer))
 
 tilesAbstract=[]
-for tiles in tileContainer:
+#for tiles in tileContainer:
     #tiles.find('div',{"class" : "tileText"})
-    tilesAbstract.append((tiles.find('div',{"class" : "tileText"})).text)
+   # tilesAbstract.append((tiles.find('a',{"class" : "tileText"})).text)
 
 articleNames = []
 articleLinks = []
 
 for tile in tileContainer:
-    articleNames.append(
-        (tile.find('div', {"class": "tileTitle"})).text.replace("\'", '').replace("\"", '').replace(' ', '').replace(
-            '-', '').replace(',', '').replace('?', '').replace(':', '').replace('.', ''))
-    articleLinks.append("https://www.morganstanley.com" + tile.find('a')['href'])
+    articleLinkVal = tile.text
+    import re
+    articleNameVal = re.sub('[^0-9a-zA-Z]+', '', articleLinkVal)
+    articleNames.append(articleNameVal)
+    #articleNames.append(tile.text.replace("\'", '').replace("\"", '').replace(' ', '').replace('-', '').replace(',', '').replace('?', '').replace(':', '').replace('.', ''))
+    if(".pdf" in tile['href']):
+        print("PDF :"+tile['href'])
+    else:
+        articleLinks.append("https://www.morganstanley.com" + tile['href'])
 
 print(articleNames)
-print(articleLinks)
+# for articleLink in articleLinks:
+#     print("\n",articleLink)
+
 
 # Create a file for each link
+#articleLinks=[]
+#articleLinks.append("https://www.morganstanley.com/im/en-us/institutional-investor/insights/macro-insights/australia-housing-bubble-starting-to-deflate.html")
 for index, link in enumerate(articleLinks):
-    import codecs
-    msimFolder = os.path.join(webDirectory, 'MSIM', articleNames[index])
-    folder = os.makedirs(msimFolder, exist_ok=True)
-    file = codecs.open(os.path.join(msimFolder, 'content.txt'), 'w+',"utf-8")
-    title = codecs.open(os.path.join(msimFolder,'title.txt'),"w+","utf-8")
-    abstract = codecs.open(os.path.join(msimFolder,'abstract.txt'),'w+',"utf-8")
-    introPara = codecs.open(os.path.join(msimFolder,'introPara.txt'),'w+',"utf-8")
+
+   # title = codecs.open(os.path.join(msimFolder,'title.txt'),"w+","utf-8")
+   # abstract = codecs.open(os.path.join(msimFolder,'abstract.txt'),'w+',"utf-8")
+   # introPara = codecs.open(os.path.join(msimFolder,'introPara.txt'),'w+',"utf-8")
     # print(link)
     html1 = urlopen(link).read()
     # print(html1)
     soup1 = BeautifulSoup(html1)
     # print(soup1)
-    content = soup1.find('div', {"class": "pageDivider"})
-    contentTitle = soup1.find('div',{"class" : "heroProductName"})
-    introParaText = soup1.find('p',{"class" : 'introCallout"'})
-    row = content.find('div', {"class": "row"})
+   # content = soup1.find('div', {"class": "pageDivider"})
 
-    data = row.findAll('div')[0]
+   # introParaText = soup1.find('p',{"class" : 'introCallout"'})
+   # row = content.find('div', {"class": "row"})
+
+    #data = row.findAll('div')[0]
     # print(content)
     # text = content.findAll('div', {"class": "col-xs-12"})[0]
     # body = text.find('div', {"id": "div_content"})
     # print(body.text)
     import re
-    dataVal = re.sub('[^0-9a-zA-Z]+', ' ', data.text)
-    file.write(dataVal)
-    dataVal = re.sub('[^0-9a-zA-Z]+', ' ', contentTitle.text)
-    title.write(contentTitle.text)
-    dataVal = re.sub('[^0-9a-zA-Z]+', ' ', tilesAbstract[index])
-    abstract.write(dataVal)
+   # dataVal = re.sub('[^0-9a-zA-Z]+', ' ', data.text)
+    #file.write(dataVal)
+  #  dataVal = re.sub('[^0-9a-zA-Z]+', ' ', contentTitle.text)
+  #  title.write(contentTitle.text)
+    #dataVal = re.sub('[^0-9a-zA-Z]+', ' ', tilesAbstract[index])
+    #abstract.write(dataVal)
+   # try:
+     #   dataVal = re.sub('[^0-9a-zA-Z]+', ' ', introParaText.text)
+     #   introPara.write(dataVal)
+  #  except:
+   #     print("No Intro available")
     try:
-        dataVal = re.sub('[^0-9a-zA-Z]+', ' ', introParaText.text)
-        introPara.write(dataVal)
+        content = soup1.find('div', {"class": "pageDivider"})
+        row = content.find('div', {"class": "row"})
+        data = row.findAll('div')[0]
+        import codecs
+
+        msimFolder = os.path.join(webDirectory, 'MSIM')
+        folder = os.makedirs(msimFolder, exist_ok=True)
+        FileNameVal = re.sub('[^0-9a-zA-Z]+', '', link)
+        fileName = FileNameVal+".txt"
+        file = codecs.open(os.path.join(msimFolder, fileName), 'w+', "utf-8")
+        contentText = data.text
+        import re
+
+        contentTextVal = re.sub('[^0-9a-zA-Z.%$]+', ' ', contentText)
+        file.write(contentTextVal)
+        file.close()
+        print(link,"=====================================================================\n")
+        print(fileName, "File Name=====================================================================")
     except:
-        print("No Intro available")
-    file.close()
-    title.close()
-    abstract.close()
-    introPara.close()
-    print("=====================================================================")
+        print("\n",link)
+   # title.close()
+   # abstract.close()
+  #  introPara.close()
 
